b5d657f918d65f1d4d3a658753ad89940a07399d
dev <dev@test.com>
2023-02-02 14:57:29 +0300

    add cors in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express,{Application} from 'express';
 import userRouter from './routes/user.route';
 import taskRouter from './routes/task.route';
+import cors from 'cors';
 import * as dotenv from 'dotenv';
 dotenv.config()
 
 const app:Application = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use('/users',userRouter);
 app.use('/tasks',taskRouter);
